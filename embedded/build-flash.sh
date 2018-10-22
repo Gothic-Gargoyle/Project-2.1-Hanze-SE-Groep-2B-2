aa039c854d5f1c0324af2a6e85d382184a2c86cf
Jelle Kaufmann <jelle@pa5am.nl>
2018-10-22 15:52:54 +0200

    Seriele communicatie/schedular voor ATMEGA328p

---
+++ embedded/build-flash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c main.c
+avr-gcc -mmcu=atmega328p main.o -o main.elf
+avr-objcopy -O ihex -R .eeprom main.elf main.hex
+avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:main.hex
+
