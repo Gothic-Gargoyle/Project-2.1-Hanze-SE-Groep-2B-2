8fd6805fc440c2ce5cfd886f34f2a86545f0172d
Joost Hobma <p.joosthobma@gmail.com>
2018-10-22 09:53:48 +0200

    init

---
+++ python/main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+class WindowController:
+
+    def __init__(self, title="window controller", width=800, ):
+        # init vars
+        self.title = title
+
+        root = Tk()
+
+        root.title(title)
+
+        root.mainloop()
+
+
+
+
+controller = WindowController()
+
+# class window controller:
+# class serial
+#
