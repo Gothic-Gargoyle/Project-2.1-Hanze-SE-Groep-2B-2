10c1f8255d8eed49367188b5904c80efe25ecc99
jossafossa <joosthobma@gmail.com>
2018-10-30 23:59:30 +0100

    handshake stuff

--- python/main2.py
+++ python/main2.py
@@ -2,6 +2,8 @@ from tkinter import *
 from threading import Timer, Thread, Event
 import serial
 import serial.tools.list_ports
+import time
+
 
 class WindowController:
 
@@ -14,8 +16,9 @@ class WindowController:
         self.root = Tk()
         self.tk_init()
 
+        # PORTCONTROLLER
         self.ports = {}
-        self.connection_loop(1)
+        self.port_controller = PortController(self.ports, callback=self.update, timeout=200)
 
         self.root.mainloop()
 
@@ -23,39 +26,53 @@ class WindowController:
         self.root.geometry(str(self.width) + "x" + str(self.height))
         self.root.title("bedieningspaneel")
 
-    def connection_loop(self, timeout):
+    def update(self):
+        for key, port in self.ports.items():
+            print(key, port)
+
+
+class PortController:
+
+    def __init__(self, ports, callback=None, timeout=1000):
+        self.ports = ports
+        self.connection_loop(timeout / 1000, callback)
+
+    def connection_loop(self, timeout, callback):
         ports = self.get_available_ports()
 
         ports_to_enable = self.get_disabled_ports(ports)
-        ports_to_disable = self.get_disconnected_ports(ports)
-        ports_to_add = self.get_unused_ports(ports)
-
-
-        print(self.ports, ports_to_add, ports_to_enable, ports_to_disable)
-
         self.enable_ports(ports_to_enable)
+
+        ports_to_disable = self.get_disconnected_ports(ports)
         self.disable_ports(ports_to_disable)
+
+        ports_to_add = self.get_unused_ports(ports)
         self.add_ports(ports_to_add)
 
-        # restart check in timout
-        Timer(timeout, lambda: self.connection_loop(timeout)).start()
+        # if the port configuration has changed
+        if ports_to_enable or ports_to_disable or ports_to_add:
+            callback()
+
+        # restart check in timeout
+        Timer(timeout, lambda: self.connection_loop(timeout, callback)).start()
 
     # returns ports that are known but disabled
     def get_disabled_ports(self, ports):
         disables_ports = []
         for port in ports:
             if port in self.ports:                                # if port is used but not active
-                if self.ports[port]["is_active"] is False:
+                if self.ports[port]["is_valid"] and self.ports[port]["is_active"] is False:
                     disables_ports.append(port)
 
         return disables_ports
 
-    # returns ports that are known and disconnected
+    # returns ports that are known, disconnected and not jet registered as disconnected
     def get_disconnected_ports(self, ports):
         disconnected_ports = []
         for port in self.ports.keys():
             if port not in ports:
-                disconnected_ports.append(port)
+                if self.ports[port]["is_active"]:
+                    disconnected_ports.append(port)
 
         return disconnected_ports
 
@@ -73,12 +90,12 @@ class WindowController:
         ports = [port.device for port in serial.tools.list_ports.comports()]
         return ports
 
-
     # enables known ports
     def enable_ports(self, ports):
         if ports:
             for port in ports:
-                self.ports[port]['is_active'] = True
+                if self.ports[port]["is_valid"]:
+                    self.ports[port]['is_active'] = True
 
     # disables known ports
     def disable_ports(self, ports):
@@ -91,10 +108,46 @@ class WindowController:
         if ports:
             for port in ports:
                 print("adding port {} to list".format(port))
-                self.ports[port] = {"is_active": True}
-                print(self.ports[port])
+                ser = serial.Serial(port, 9600, timeout=.100)
+                self.ports[port] = {"is_valid": False, "is_active": False, "serial": ser}
+                valid = self.handshake_sequence(ser)
+                if valid:
+                    self.ports[port]["is_valid"] = True
+                    self.ports[port]["is_active"] = True
+                    print(self.ports[port])
 
+    def handshake_sequence(self, ser):
+        for i in range(10):
+            valid = self.handshake(ser, '!connectie-check\r', "Temperatuurmeetsensor v0.1")
+            if valid:
+                return True
+        return False
 
+    # does a handshake with a device on a specific port
+    def handshake(self, ser, command, valid):
+        _command = bytes(command, encoding="utf-8")
+        print("sending !connectie-check to", ser.name)
+        ser.write(_command)
+        return True if self.read(ser).strip() == valid else False
+
+    # returns readdata from a device on a given comport
+    def read(self, ser):
+        eol = b'\r'
+        leneol = len(eol)
+        line = bytearray()
+        while True:
+            c = ser.read(1)
+            if c:
+                line += c
+                if line[-leneol:] == eol:
+                    break
+            else:
+                break
+        try:
+            line.decode(encoding="utf-8")
+        except:
+            return "@ongeldig\n"
+        return line.decode(encoding="utf-8")
 
 
 controller = WindowController()
