b291ecb8dfa9b65da379635cd8dfb435528af19d
Allard <allard1997@users.noreply.github.com>
2018-10-31 11:30:38 +0100

    Code cleanup

--- python/LDR04.py
+++ python/LDR04.py
@@ -1,39 +1,36 @@
-import serial                   #import serial library
-import numpy as np              #import numpy
-import matplotlib.pyplot as plt #import matplotlib library
+import serial
+import matplotlib.pyplot as plt
 import datetime
 from drawnow import *
 
+arduinoData = serial.Serial('COM9', 9600)
+plt.ion()
+cnt = 0
 light = []
 x = []
-arduinoData = serial.Serial('COM9', 9600) #Creating our serial object named arduinoData
-plt.ion() #Tell matplotlib you want interactive mode to plot live data
-cnt=0
 
-def makeFig():             #Create a function that makes our desired plot
-    plt.ylim(0,100)                                        #Set y min and max values
+
+def makeFig():
+    plt.ylim(0, 100)
     plt.gcf().autofmt_xdate()
-    plt.title('Real-time licht percentage: {}'.format(light[-1]))             #Plot the title
-    plt.grid(True)                                         #Turn the grid on
-    plt.ylabel('Licht percentage')                                   #Set ylabels
+    plt.title('Real-time licht percentage: {}%'.format(light[-1]))
+    plt.grid(True)
+    plt.ylabel('Licht percentage')
     plt.xlabel('Tijd')
-    plt.plot(x, light, 'r-', label='Licht percentage')       #plot the temperature
-    plt.legend(loc='upper left')                           #plot the legend
+    plt.plot(x, light, 'r-', label='Licht percentage')
+    plt.legend(loc='upper left')
 
-while True: # While loop that loops forever
-    while (arduinoData.inWaiting()==0):    #Wait here until there is data
-        pass #do nothing
-    arduinoString = arduinoData.readline().decode('utf-8') #read the line of text from the serial port
+while True:
+    while (arduinoData.inWaiting() == 0):
+        pass
+    arduinoString = arduinoData.readline().decode('utf-8')
     light_value = int(arduinoString)
     temp_value = light_value / 1024 * 100
     value = float("{0:.2f}".format(temp_value))
-    light.append(value)      #Build our temperatureF array by appending temp readings
+    light.append(value)
     x.append(datetime.datetime.now().strftime("%H:%M:%S"))
-    print(light)
-    # print(x)
-    drawnow(makeFig)                       #Call drawnow to update our live graph
-    # plt.pause(.000001)                     #Pause Briefly. Important to keep drawnow from crashing
-    cnt=cnt+1
-    if(cnt>720):                            #If you have 50 or more points, delete the first one from the array
-        light.pop(0)                #This allows us to just see the last 50 data points
- 
+    drawnow(makeFig)
+    plt.pause(.000001)
+    cnt = cnt+1
+    if(cnt > 720):
+        light.pop(0)
--- python/TMP36.py
+++ python/TMP36.py
@@ -1,37 +1,34 @@
-import serial                   #import serial library
-import numpy as np              #import numpy
-import matplotlib.pyplot as plt #import matplotlib library
+import serial
+import matplotlib.pyplot as plt
 import datetime
 from drawnow import *
 
+arduinoData = serial.Serial('COM9', 9600)
+plt.ion()
+cnt = 0
 temperatureC = []
 x = []
-arduinoData = serial.Serial('COM9', 9600) #Creating our serial object named arduinoData
-plt.ion() #Tell matplotlib you want interactive mode to plot live data
-cnt=0
 
-def makeFig():             #Create a function that makes our desired plot
-    plt.ylim(-50,50)                                        #Set y min and max values
+
+def makeFig():
+    plt.ylim(-50, 50)
     plt.gcf().autofmt_xdate()
-    plt.title('Real-time temperatuur: {}'.format(temperatureC[-1]))             #Plot the title
-    plt.grid(True)                                         #Turn the grid on
-    plt.ylabel('Temp C')                                   #Set ylabels
+    plt.title('Real-time temperatuur: {}°C'.format(temperatureC[-1]))
+    plt.grid(True)
+    plt.ylabel('Temp C')
     plt.xlabel('Tijd')
-    plt.plot(x, temperatureC, 'r-', label='Graden C')       #plot the temperature
-    plt.legend(loc='upper left')                           #plot the legend
+    plt.plot(x, temperatureC, 'r-', label='Graden C')
+    plt.legend(loc='upper left')
 
-while True: # While loop that loops forever
-    while (arduinoData.inWaiting()==0):    #Wait here until there is data
-        pass #do nothing
-    arduinoString = arduinoData.readline(5).decode('utf-8') #read the line of text from the serial port
+while True:
+    while (arduinoData.inWaiting() == 0):
+        pass
+    arduinoString = arduinoData.readline(5).decode('utf-8')
     temp_value = float(arduinoString)
-    temperatureC.append(temp_value)      #Build our temperatureF array by appending temp readings
+    temperatureC.append(temp_value)
     x.append(datetime.datetime.now().strftime("%H:%M"))
-    print(temperatureC)
-    print(x)
-    drawnow(makeFig)                       #Call drawnow to update our live graph
-    plt.pause(.000001)                     #Pause Briefly. Important to keep drawnow from crashing
-    cnt=cnt+1
-    if(cnt>720):                            #If you have 50 or more points, delete the first one from the array
-        temperatureC.pop(0)                #This allows us to just see the last 50 data points
- 
+    drawnow(makeFig)
+    plt.pause(.000001)
+    cnt = cnt+1
+    if(cnt > 720):
+        temperatureC.pop(0)
